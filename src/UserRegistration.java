ea334ee3a3f06e27b2e99446cdc799d4f4c87938
aniketbudhbaware <aniketbudhbaware111@gmail.com>
2021-11-11 23:02:17 +0530

    user registration problems added

---
+++ src/UserRegistration.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//UserRegistration class for user details validation
+
+public class UserRegistration {
+	   //First name starts with Cap and has minimum 3 characters validation
+    public boolean validFirstName(String firstName) throws RegistrationException {
+        String regex = "^[A-Z][A-Za-z]{2,15}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(firstName);
+        if( matcher.matches() == false ) {
+            throw new RegistrationException("Invalid First Name");
+        }
+        return matcher.matches();
+    }
+
+    //last name starts with Cap and has minimum 3 characters validation
+    public boolean validLastName(String lastName) throws RegistrationException {
+        String regex = "^[A-Z][A-Za-z]{2,15}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(lastName);
+        if( matcher.matches() == false ) {
+            throw new RegistrationException("Invalid Last Name");
+        }
+        return matcher.matches();
+    }
+
+    //validating all valid emails
+    public boolean validEmail(String Email) throws RegistrationException {
+        String regex = "^\\w+([+-_.][\\w])*@[\\w]+\\.(\\w{2,4}\\.)?[^\\d]{2,4}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(Email);
+        if( matcher.matches() == false ) {
+            throw new RegistrationException("Invalid Email");
+        }
+        return matcher.matches();
+    }
+
+    //validating Mobile Format-E.g. 91 9919819801-Country code follow by space
+    public boolean validPhoneNum(String number) throws RegistrationException {
+        String regex = "^[1-9][0-9]\\s[0-9]{10}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(number);
+        if( matcher.matches() == false ) {
+            throw new RegistrationException("Invalid Phone Number");
+        }
+        return matcher.matches();
+    }
+
+    //rule4 for valid pass: minimum 8 characters, should have atleast 1 upper-case char, atleast 1 numeric num. and have exactly 1 special char.
+    public boolean validPassword(String password) throws RegistrationException {
+        String rule4 = "^(?=.{8,20}$)(?=.*[0-9])(?=.*[A-Z])[A-Za-z0-9]*[.@#$%^&*!][A-Za-z0-9]*$";
+        Pattern pattern = Pattern.compile(rule4);
+        Matcher matcher = pattern.matcher(password);
+        if( matcher.matches() == false ) {
+            throw new RegistrationException("Invalid Password");
+        }
+        return matcher.matches();
+    }
+
+
+}
---
+++ src/UserRegistrationTest.java
@@ -0,0 +1,109 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserRegistrationTest {
+	 UserRegistration userRegistration = new UserRegistration();
+
+	    //Happy test case for first-name
+	    @Test
+	    public void givenFirstName_WhenValid_ShouldReturnTrue() {
+	        boolean value = false;
+	        try {
+	            value = userRegistration.validFirstName("Shubham");
+	        } catch (RegistrationException e) {
+	            e.printStackTrace();
+	        }
+	        Assertions.assertEquals(true, value);
+	    }
+
+	    //Sad test case for first_name
+	    @Test
+	    public void givenFirstName_WhenNotValid_shouldThrowAnException() {
+
+	        Assertions.assertThrows(RegistrationException.class, ()->
+	            userRegistration.validFirstName("shubham")
+	        );
+	    }
+
+	    //Happy test case for last-name
+	    @Test
+	    public void givenLastName_whenValid_shouldReturnTrue() {
+	        boolean value = false;
+	        try {
+	            value = userRegistration.validLastName("Kumar");
+	        } catch (RegistrationException e) {
+	            e.printStackTrace();
+	        }
+	        Assertions.assertEquals(true, value);
+	    }
+
+	    //sad test case for last-name
+	    @Test
+	    public void givenLastName_whenNotValid_shouldThrowAnException() {
+	        Assertions.assertThrows(RegistrationException.class, ()->
+	            userRegistration.validLastName("kumar")
+	        );
+	    }
+
+	    //Happy test case for Email
+	    @Test
+	    public void givenEmail_whenValid_shouldReturnTrue() {
+	        boolean value = false;
+	        try {
+	            value = userRegistration.validEmail("Trump123@bl.co.in");
+	        } catch (RegistrationException e) {
+	            e.printStackTrace();
+	        }
+	        Assertions.assertEquals(true, value);
+	    }
+
+	    //sad test case for Email
+	    @Test
+	    public void givenEmail_whenNotValid_shouldThrowAnException() {
+	        Assertions.assertThrows(RegistrationException.class, ()->
+	            userRegistration.validEmail("shubham@123.co.in.in")
+	        );
+	    }
+
+	    //Happy test case for phone number
+	    @Test
+	    public void givenPhoneNum_whenValid_shouldReturnTrue() {
+	        boolean value = false;
+	        try {
+	            value = userRegistration.validPhoneNum("91 7879767574");
+	        } catch (RegistrationException e) {
+	            e.printStackTrace();
+	        }
+	        Assertions.assertEquals(true, value);
+	    }
+
+	    //sad test case for phone number
+	    @Test
+	    public void givenPhoneNum_whenNotValid_shouldThrowAnException() {
+	        Assertions.assertThrows(RegistrationException.class, ()->
+	            userRegistration.validPhoneNum("9173544333")
+	        );
+	    }
+
+	    //Happy test case for password
+	    @Test
+	    public void givenPassword_whenValid_shouldReturnTrue() {
+	        boolean value = false;
+	        try {
+	            value = userRegistration.validPassword("retake@Action2");
+	        } catch (RegistrationException e) {
+	            e.printStackTrace();
+	        }
+	        Assertions.assertEquals(true, value);
+	    }
+
+	    //sad test case for password
+	    @Test
+	    public void givenPassword_whenNotValid_shouldThrowAnException() {
+	        Assertions.assertThrows(RegistrationException.class, ()->
+	            userRegistration.validFirstName("shubham@123")
+	        );
+	    }
+
+
+}
